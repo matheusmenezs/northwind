e5d9bf0d445f4aaa02644fc58c838f7b6074abdd
matheusmenezs <matheushm1798@gmail.com>
2022-05-18 21:27:34 -0300

    feat: add alter method

--- config.py
+++ config.py
@@ -12,3 +12,28 @@ class config:
             'user': 'postgres',
             'password': 'postgres'
         }
+
+    def alterDatabase(self, query, values):
+        conn = None
+        try:
+            # connect to the PostgreSQL database
+            connection = psycopg2.connect(
+                config.setParameters(self).connectiondata)
+            # create a new cursor (open a session)
+            session = connection.cursor()
+
+            # execute the command in ram
+            session.execute(query, values)
+
+            # commit the changes to the database
+            connection.commit()
+
+            # close the session
+            session.close()
+
+        except psycopg2.Error:
+            return psycopg2.Error
+        finally:
+            if conn is not None:
+                conn.close()
+        return 'Success'
