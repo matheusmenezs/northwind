df56fcb697f331b70961296e194383bfb0fc6b5f
Renan Vítor <renanpiranguinho@gmail.com>
2022-05-20 15:55:24 -0300

    feat: add controller methods

--- ex01/controller.py
+++ ex01/controller.py
@@ -12,6 +12,22 @@ class Controller:
                 #order = OrderM.createOrder(l)
                 status = OrderM.registerOrder('')
                 self.view.printStatus(status)
+            if option == 2:
+                id = self.view.getOrderCod()
+                order = OrderM.readOrder(id)
+                self.view.printStatus(order)
+            if option == 3:
+                id = self.view.getOrderCod()
+                status = OrderM.deleteOrder(id)
+                self.view.printStatus(status)
+            if option == 4:
+                id = self.view.getOrderCod()
+                order = OrderM.readOrder(id)
+                self.view.printOrder(order)
+                if(order is not None):
+                    l = self.view.getOrderDataUpdate(id)
+                    status = OrderM.updateOrder(l)
+                    self.view.printStatus(status)
             option = self.view.main()
 
     def __init__(self):
